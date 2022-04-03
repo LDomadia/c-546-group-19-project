0e20d77ad0edba06cd5fc8a4029824d6277258f0
AlephEleven <lambdasauce2@gmail.com>
2022-04-03 07:19:17 -0400

    added sign up status to main.handlebars

--- routes/closet.js
+++ routes/closet.js
@@ -6,7 +6,9 @@ const closetData = data.closet;
 // GET /
 router.get('/', async (req, res) => {
     try {
-      res.render('pages/single/index', {title: "Digital Closet"});
+      res.render('pages/single/index', {title: "Digital Closet",
+                                        signed_in: true,
+                                        home_not_signed_in: false});
     } catch (e) {
       res.sendStatus(500);
     }
