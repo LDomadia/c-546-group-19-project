913615b068bfae47cdb3e2069fb6ebec7b79dace
Leena Domadia <leena.domadia2018@gmail.com>
2022-04-05 17:00:50 -0400

    added a favicon (it's pretty ugly though..)

--- public/scripts/clothes_script.js
+++ public/scripts/clothes_script.js
@@ -2,21 +2,16 @@ const image = document.getElementById('placeholder-img');
 const imageBtn = document.getElementById('img-btn');
 
 if (imageBtn) {
-    imageBtn.addEventListener("change", function() {
-        changeImage(this);
-    })
+    imageBtn.addEventListener("change", changeImage);
 }
 
 function changeImage(input) {
     var reader;
-
-  if (input.files && input.files[0]) {
-    reader = new FileReader();
-
-    reader.onload = function(e) {
-      image.setAttribute('src', e.target.result);
+    if (input.files && input.files[0]) {
+        reader = new FileReader();
+        reader.onload = function(e) {
+            image.setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
     }
-
-    reader.readAsDataURL(input.files[0]);
-  }
 }
