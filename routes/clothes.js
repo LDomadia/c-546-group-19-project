8d1368ccf8b05fd00c201a27265c142c87851238
Leena Domadia <leena.domadia2018@gmail.com>
2022-04-05 14:29:23 -0400

    added stylesheet link

--- routes/clothes.js
+++ routes/clothes.js
@@ -6,7 +6,12 @@ router.route('/').get(async (req, res) => {
 })
 
 router.route('/new').get(async (req, res) => {
-    res.render('pages/medium/clothingNew', {title: 'Add New Clothing', clothesPage: true})
+    res.render('pages/medium/clothingNew', {
+        title: 'Add New Clothing',
+        clothesPage: true,
+        stylesheet: '/public/styles/clothes_styles.css',
+        script: '/public/scripts/clothes_script.js'
+    })
 })
 
 module.exports = router;
