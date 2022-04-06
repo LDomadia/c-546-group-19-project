78c3c6fd2c947f41b5b5a33368d933963c9961f6
Leena Domadia <leena.domadia2018@gmail.com>
2022-04-05 20:44:34 -0400

    color/patterns work

--- public/scripts/clothes_script.js
+++ public/scripts/clothes_script.js
@@ -1,9 +1,18 @@
 const image = document.getElementById('placeholder-img');
 const imageBtn = document.getElementById('img-btn');
+const colorPatternList = document.getElementById('colors-patterns-list');
+const colorPatternInput = document.getElementById('colors-patterns-input');
+const colorPatternBtn = document.getElementById('colors-patterns-btn');
 
 if (imageBtn) {
     imageBtn.addEventListener("change", function() {
         changeImage(this);
+    });
+}
+
+if (colorPatternBtn) {
+    colorPatternBtn.addEventListener("click", function() {
+        addToList(colorPatternInput, colorPatternList, 'colors-patterns[]');
     });
 }
 
@@ -16,4 +25,17 @@ function changeImage(input) {
         }
         reader.readAsDataURL(input.files[0]);
     }
+}
+
+function addToList(input, list, listName) {
+    let inputValue = input.value.trim();
+    // check if input is valid
+    let liItem = document.createElement('li')
+    liItem.innerHTML = inputValue;
+    let hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', listName);
+    hiddenInput.setAttribute('value', inputValue);
+    liItem.appendChild(hiddenInput);
+    list.appendChild(liItem);
 }
