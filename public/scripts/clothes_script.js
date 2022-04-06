abe04ba8a388f68c135e2a2ad6655c4e349ea285
Leena Domadia <leena.domadia2018@gmail.com>
2022-04-06 18:06:17 -0400

    added delete icon to chip

--- public/scripts/clothes_script.js
+++ public/scripts/clothes_script.js
@@ -38,16 +38,23 @@ function changeImage(input) {
 
 function addToList(input, list, listName) {
     let inputValue = input.value.trim();
-    // check if input is valid
+    // check if input is valid first here
     let btn = document.createElement('button');
-    btn.innerHTML = 'X';
     btn.setAttribute('type', 'button');
+    btn.setAttribute('class', 'chip-btn');
+    btn.setAttribute('aria-label', 'delete ' + inputValue);
+    btn.setAttribute('title', 'delete ' + inputValue);
+
+    let icon = document.createElement('i');
+    icon.setAttribute('class', 'fa-solid fa-circle-xmark');
+
     let liItem = document.createElement('li')
     liItem.innerHTML = inputValue;
     let hiddenInput = document.createElement('input');
     hiddenInput.setAttribute('type', 'hidden');
     hiddenInput.setAttribute('name', listName);
     hiddenInput.setAttribute('value', inputValue);
+    btn.appendChild(icon);
     liItem.appendChild(hiddenInput);
     liItem.appendChild(btn);
     list.appendChild(liItem);
