466062a139b4c6cf19af448845c78b249e653b7d
uityh <matthew6600@gmail.com>
2022-04-15 18:24:33 -0400

    Added middleware to prevent signup and login headers from being displayed

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+//Middleware
+router.use("/", (req, res, next) => {
+  if (req.session.user) {
+    return res.render("pages/single/index", {
+      title: "Digital Closet",
+      homePage: true,
+      logged_in: true,
+    });
+  }
+  next();
+});
+
 // GET /
 router.get("/", async (req, res) => {
   try {
