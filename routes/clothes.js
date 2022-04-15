ba6c997bcb66e835dd839be5439255d313bbb84d
uityh <matthew6600@gmail.com>
2022-04-15 19:22:38 -0400

    Added middleware to redirect to login page if user is not logged in

--- routes/clothes.js
+++ routes/clothes.js
@@ -1,17 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+//Middleware
+router.use("/", (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect("/account/login");
+  }
+  next();
+});
 
-router.route('/').get(async (req, res) => {
-    res.render('pages/results/clothings', {title: 'My Clothes', clothesPage: true})
-})
+router.route("/").get(async (req, res) => {
+  if (!req.session.user)
+    return res.render("pages/results/clothings", {
+      title: "My Clothes",
+      clothesPage: true,
+      not_logged_in: true,
+    });
+  res.render("pages/results/clothings", {
+    title: "My Clothes",
+    clothesPage: true,
+  });
+});
 
-router.route('/new').get(async (req, res) => {
-    res.render('pages/medium/clothingNew', {
-        title: 'Add New Clothing',
-        clothesPage: true,
-        stylesheet: '/public/styles/clothes_styles.css',
-        script: '/public/scripts/clothes_script.js'
-    })
-})
+router.route("/new").get(async (req, res) => {
+  res.render("pages/medium/clothingNew", {
+    title: "Add New Clothing",
+    clothesPage: true,
+    stylesheet: "/public/styles/clothes_styles.css",
+    script: "/public/scripts/clothes_script.js",
+  });
+});
 
 module.exports = router;
