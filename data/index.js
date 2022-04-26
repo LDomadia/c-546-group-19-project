c5d45924a4546c66a25e6311313d32f252244acf
AlephEleven <lambdasauce2@gmail.com>
2022-04-26 10:23:19 -0400

    added func to index

--- data/index.js
+++ data/index.js
@@ -1,4 +1,5 @@
 //update to whatever functions we need
+const gen_outfitData = require("./gen_outfit");
 const commentsData = require("./comments");
 const clothesData = require("./clothes");
 const outfitsData = require("./outfits");
@@ -6,6 +7,7 @@ const accountData = require("./account");
 
 module.exports = {
   //export list
+  gen_outfit: gen_outfitData,
   comments: commentsData,
   clothes: clothesData,
   outfits: outfitsData,
