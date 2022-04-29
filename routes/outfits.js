e9d1de0151221c5d0a4db0f8f116aa9bb0f0221b
uityh <matthew6600@gmail.com>
2022-04-29 00:37:49 -0400

    Created outfits route

---
+++ routes/outfits.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const outfitsData = require("../data/outfits");
+const multer = require("multer");
+
+const upload = multer({ dest: "uploads/" });
+
+//Middleware
+router.use("/", (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect("/account/login");
+  }
+  next();
+});
+
+router.route("/").get(async (req, res) => {
+  try {
+    let outfitItems = await outfitsData.getOutfitItems(
+      req.session.user.username
+    );
+    res.render("pages/results/outfits", {
+      title: "My Outfits",
+      outfitsPage: true,
+      outfitItems: outfitItems,
+    });
+  } catch (e) {
+    res.status(500).render("pages/results/outfits", {
+      title: "My Outfits",
+      outfitsPage: true,
+      error: e,
+    });
+  }
+});
