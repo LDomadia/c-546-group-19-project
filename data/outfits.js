029e9655ee3b6a6eef788b95e44edbd13fcc5816
uityh <matthew6600@gmail.com>
2022-04-30 22:20:39 -0400

    Finished add new outfit db function

--- data/outfits.js
+++ data/outfits.js
@@ -27,10 +27,6 @@ const errors_strlist = function (lst, name) {
     throw `${name} is not initialized`;
   }
 
-  if (lst.length < 1) {
-    throw `${name} cannot be empty list`;
-  }
-
   return lst.map((a) => errors_string(a));
 };
 
@@ -41,38 +37,58 @@ module.exports = {
     const userDocument = await usersCollection.findOne({ username: username });
     return userDocument.userOutfits;
   },
-  async addNewOutfits(creator, status, outfitName, season, style) {
+  async addNewOutfits(creator, clothes, status, outfitName, season, style) {
     let err = function (str) {
       return `Error: ${str} was not provided`;
     };
-    let arg_names = ["creator", "status", "outfitName", "season", "style"];
+    let arg_names = [
+      "creator",
+      "clothes",
+      "status",
+      "outfitName",
+      "season",
+      "style",
+    ];
     for (let i = 0; i < arg_names.length; i++) {
       if (!arguments[i]) {
         throw err(arg_names[i]);
       }
     }
-
+    if (typeof creator !== "string") {
+      throw "Error: creator should be a string";
+    }
+    if (typeof status !== "string") {
+      throw "Error: status should be a string";
+    }
+    if (typeof outfitName !== "string") {
+      throw "Error: outfit name should be a string";
+    }
     creator = errors_string(creator, "creator");
     status = errors_string(status, "status");
     outfitName = errors_string(outfitName, "outfitName");
 
     season = errors_strlist(season, "season");
     style = errors_strlist(style, "style");
+    clothes = errors_strlist(clothes, "clothes");
 
     const outfitsCollection = await outfits();
     const existingOutfits = await outfitsCollection.findOne({
-      outfitName: { $regex: "^" + outfitName + "$", $options: "i" },
+      outfitName: {
+        $regex:
+          "/^" + outfitName.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") + "/$/",
+        $options: "i",
+      },
     });
     if (existingOutfits != null) throw "Error: name is already taken";
 
     let newOutfits = {
       creator: creator,
+      clothes: clothes,
       likes: 0,
       status: status,
       outfitName: outfitName,
       season: season,
       style: style,
-      clothes: [],
       comments: [],
     };
     const insertInfo = await outfitsCollection.insertOne(newOutfits);
