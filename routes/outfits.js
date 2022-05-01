fa98a19474038d286b8fc0df72e7ed14fd9502e4
uityh <matthew6600@gmail.com>
2022-04-30 22:42:05 -0400

    Added message on successful outfit creation

--- routes/outfits.js
+++ routes/outfits.js
@@ -51,6 +51,16 @@ router.route("/new").get(async (req, res) => {
     });
   }
 });
+router.route("/success").post(async (req, res) => {
+  let outfitItems = 1;
+  res.render("pages/results/outfits", {
+    title: "My Outfits",
+    outfitsPage: true,
+    outfitItems: outfitItems,
+    successMsg: "Outfit has successfuly been added!",
+  });
+});
+
 router.route("/new").post(async (req, res) => {
   //console.log(req.body);
   try {
@@ -74,6 +84,7 @@ router.route("/new").post(async (req, res) => {
       title: "My Outfits",
       outfitsPage: true,
       outfitItems: outfitItems,
+      successMsg: "Outfit has successfuly been added!",
     });
   } catch (e) {
     res.status(500).render("pages/results/outfits", {
