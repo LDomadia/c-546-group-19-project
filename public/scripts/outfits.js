f1581ec332705f9bcf0f8582d10751a9de984dd3
uityh <matthew6600@gmail.com>
2022-05-05 14:58:45 -0400

    Created client side script for outfits page

---
+++ public/scripts/outfits.js
@@ -0,0 +1,5 @@
+const carouselImages = document.getElementsByClassName("carousel-inner");
+
+for (let i = 0; i < carouselImages.length; i++) {
+  carouselImages[i].firstElementChild.classList.add("active");
+}
