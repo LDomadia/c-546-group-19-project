0865159dea46b2f89b3cbb8cafb88c9da6cc2930
Leena Domadia <leena.domadia2018@gmail.com>
2022-05-05 18:19:47 -0400

    using jquery

--- public/scripts/clothes_all.js
+++ public/scripts/clothes_all.js
@@ -1,28 +1,57 @@
-const deleteBtns = document.getElementsByClassName('delete-btn');
+// const deleteBtns = document.getElementsByClassName('delete-btn');
 
-if (deleteBtns) {
-    for (let i = 0; i < deleteBtns.length; i++) {
-        deleteBtns[i].addEventListener('click', function() {
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    let id = this.parentElement.parentElement.id
-                    id = id.substring(1)
-                    
-                    Swal.fire(
-                        'Deleted!',
-                        'Your file has been deleted.',
-                        'success'
-                    )
-                }
+// if (deleteBtns) {
+//     for (let i = 0; i < deleteBtns.length; i++) {
+//         deleteBtns[i].addEventListener('click', function() {
+//             Swal.fire({
+//                 title: 'Are you sure?',
+//                 text: "You won't be able to revert this!",
+//                 icon: 'warning',
+//                 showCancelButton: true,
+//                 confirmButtonColor: '#3085d6',
+//                 cancelButtonColor: '#d33',
+//                 confirmButtonText: 'Yes, delete it!'
+//             }).then((result) => {
+//                 if (result.isConfirmed) {
+//                     let id = this.parentElement.parentElement.id.substring(1);
+//                     console.log(id);
+//                     Swal.fire(
+//                         'Deleted!',
+//                         'Your file has been deleted.',
+//                         'success'
+//                     )
+//                 }
+//             })
+//         })
+//     }
+// }
+
+(function($) {
+    const deleteBtns = $('.delete-btn');
+    console.log(deleteBtns);
+    if (deleteBtns) {
+        for (let i = 0; i < deleteBtns.length; i++) {
+            deleteBtns[i].addEventListener('click', function() {
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        let id = this.parentElement.parentElement.id.substring(1);
+                        console.log(id);
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+                })
             })
-        })
+        }
     }
-}
+})(window.jQuery);
